a18d023e42f052590bbd5b046dca0a469552d3c0
danielkeyti <danielkeyti@fba37922-c192-2a77-1540-a3b36e92d0da>
2013-02-27 01:26:07 +0000

    nova pasta linguagem, duas classes exercicios

---
+++ daniel/Unifil/linguagem/aula_26_02/Dog.java
@@ -0,0 +1,12 @@
+package aula_26_02;
+
+public class Dog {
+
+	private int tamanho;
+	private String raca;
+	private String nome;
+	
+	public void latir(){
+		System.out.println("Au Au Au! ");
+	}
+}
---
+++ daniel/Unifil/linguagem/aula_26_02/teste.java
@@ -0,0 +1,10 @@
+package aula_26_02;
+
+public class teste {
+
+	public static void main(String[] args) {
+		Dog d = new Dog();
+		d.latir();
+	}
+
+}
