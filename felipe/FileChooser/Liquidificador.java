758faf1e41d1c29792f73474839a4dc8f8a91bde
felipeenju@gmail.com <felipeenju@gmail.com@fba37922-c192-2a77-1540-a3b36e92d0da>
2013-03-09 16:54:47 +0000

    Liquidificador

---
+++ felipe/FileChooser/Liquidificador.java
@@ -0,0 +1,37 @@
+
+public class Liquidificador {
+	private int velocidade;
+	private int velocidadeMaxima;
+	private char estado;
+	
+	
+	public Liquidificador(){
+		this.estado = 'D';
+		this.velocidadeMaxima = 5;
+	}
+	
+	public void setVelocidade(int V){
+		velocidade = V;
+	}
+	
+	public void setVelocidadeMaxima(int Vm){
+		velocidadeMaxima = Vm;
+	}
+	
+	public char getEstado(){
+		return estado;
+	}
+	
+	public void setEstado(char St){
+		estado = St;
+	}
+	
+	public int getVelocidade(){
+		return velocidade;
+	}
+	
+	public int getVelocidadeMaxima(){
+		return velocidadeMaxima;
+	}
+
+}
