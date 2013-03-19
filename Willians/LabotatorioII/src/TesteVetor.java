47dd77ffea6ae939fbc8b3f823c22fea91abfbcf
williansferr <williansferr@fba37922-c192-2a77-1540-a3b36e92d0da>
2013-03-19 22:29:06 +0000

    realizado alteracao

--- Willians/LabotatorioII/src/TesteVetor.java
+++ Willians/LabotatorioII/src/TesteVetor.java
@@ -6,14 +6,14 @@ public class TesteVetor {
 	
 	public static void main(String[] args) {
 	
-		Vector vector1 = new Vector(5);
-		vector1.addElement("Willians");
+		Vector vector1 = new Vector();
+		vector1.addElement("Willians".toUpperCase());
 		vector1.addElement("Camila");
 		
 		System.out.println("Numero de componetes do vetor (Size): " + vector1.size());
 		System.out.println("capacity: "+ vector1.capacity());
 		
-		if(vector1.removeElement("Willians")){
+		if(vector1.removeElement("WiLlians".toUpperCase())){
 			System.out.println("Removido 1");
 			
 		}else{
