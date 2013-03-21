0d87f4d14f667d005b2fd61ec09cc5f36076d6b1
danielkeyti <danielkeyti@fba37922-c192-2a77-1540-a3b36e92d0da>
2013-03-21 19:41:52 +0000

    exercicio de "Turma" - laboratorio

---
+++ Daniel/Laboratorio/marco_21/ex_21_03/Turma.java
@@ -0,0 +1,86 @@
+package ex_21_03;
+/*Exercício:
+
+Crie a classe Turma de acordo com as definições abaixo: 
+
+1)	Atributos privados:
+a.	turma – String - Nome da turma.
+b.	turno – String - Nome do turno. 
+c.	aluno – Vector - Aluno.
+
+2)	Construtor sem parâmetro – instanciar o vector.
+
+3)	Construtor com parâmetro nome da turma – inicializar atributo turma e instanciar o vector.
+
+4)	Métodos públicos set e get nos atributos turma e turno.
+
+5)	Método público para matricular aluno – adicionar aluno no vector
+
+6)	Método público para cancelar matricula – excluir aluno do vector.
+
+7)	Método público para obter a quantidade de alunos matriculados.
+
+8)	Método público para listar alunos da turma. 
+*/
+import java.util.Enumeration;
+import java.util.Vector;
+
+import javax.swing.JOptionPane;
+
+public class Turma {
+	private String nomeTurma;
+	private String nomeTurno;
+	private Vector<String> aluno;
+	
+	public Turma(){
+	}
+	
+	public Turma(String turma){
+		this.nomeTurma = turma;
+		aluno = new Vector<String>();
+	}
+	
+	public String getNomeTurma(){
+		return this.nomeTurma;
+	}
+	
+	public void setNomeTurma(String nomeTurma){
+		this.nomeTurma = nomeTurma;
+	}
+
+	public String getNomeTurno() {
+		return nomeTurno;
+	}
+
+	public void setNomeTurno(String nomeTurno) {
+		this.nomeTurno = nomeTurno;
+	}
+	
+	public void matricular(String nomeAluno){
+		this.aluno.addElement(nomeAluno);
+	}
+	
+	public void cancelarMatricula(String nomeAluno){
+		if(aluno.indexOf(nomeAluno) < 0){
+			JOptionPane.showMessageDialog(null, "Aluno não existe");
+		}
+		aluno.removeElement(nomeAluno);
+	}
+	
+	public void quantidadeAlunos(){
+		System.out.println(aluno.size() + " Alunos matriculados");
+	}
+	
+	public void listarAlunos(){
+		Enumeration<String> enume = aluno.elements();
+		for (int i = 0; i < aluno.capacity(); i++) {
+			try {
+				System.out.println(enume.nextElement());
+			} catch (Exception e) {
+				aluno.trimToSize();
+			}
+		}
+	}
+	
+	
+}
---
+++ Daniel/Laboratorio/marco_21/ex_21_03/testeTurma.java
@@ -0,0 +1,34 @@
+package ex_21_03;
+
+import javax.swing.JOptionPane;
+
+public class testeTurma {
+
+	public static void main(String[] args) {
+		Turma turma = new Turma("Sis");
+		
+//		turma.setNomeTurma(JOptionPane.showInputDialog("Turma"));
+//		turma.setNomeTurno(JOptionPane.showInputDialog("Turno"));
+//		JOptionPane.showMessageDialog(null, turma.getNomeTurma());
+//		JOptionPane.showMessageDialog(null, turma.getNomeTurno());
+
+		int n = JOptionPane.YES_OPTION;
+		
+		while(n == JOptionPane.YES_OPTION){
+			turma.matricular(JOptionPane.showInputDialog("Nome do Aluno"));
+			n = JOptionPane.showConfirmDialog(null, "Deseja continuar?");
+		}
+		
+		n = JOptionPane.YES_OPTION;
+		
+		while(n == JOptionPane.YES_OPTION){
+			turma.cancelarMatricula(JOptionPane.showInputDialog("Informe o aluno a ser excluido/cancelando"));
+			n = JOptionPane.showConfirmDialog(null, "Deseja continuar?");
+		}
+		
+		turma.quantidadeAlunos();
+		turma.listarAlunos();
+		
+	}
+
+}
